9f1ee240365a052bbf2e36a156b3852e7b284a2a
panzhaodong@appgame.com <panzhaodong@appgame.com>
2017-09-14 12:11:46 +0800

    add some things

--- src/Ford/Escape/Escape2017.php
+++ src/Ford/Escape/Escape2017.php
@@ -3,6 +3,7 @@ namespace Ford\Escape;
 
 class Escape2017{
 	public static function info(){
-	echo "it is ford Escape2017";
+		echo "it is ford Escape2017";
+		echo 'you can you up, no can no bibi';
 	}
 }
